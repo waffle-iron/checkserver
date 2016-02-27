7d50bb76026b038cc92dfaa22848115057640a94
leomaron <maronileo@gmail.com>
2016-02-27 14:55:19 +0100

    First Version

---
+++ checkserver.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+
+var todolist = [
+  {
+    name:"Bügeln", complete: false,
+    name:"Waschen", complete: false
+  }
+]
+
+app.get('/', function (req, res) {
+    res.send(todolist);
+  });
+
+app.get('/update', function(req, res) {
+  if(req.query.name && req.query.complete) {
+    var index = todolist.findIndex(function(item) {
+      if (item.name == req.query.name) {
+        return true;
+      }else{
+        return false;
+      }
+    })
+
+    var newItem = {
+    name: req.query.name,
+    complete: req.query.complete
+    }
+
+    if (index == -1) {
+    todolist.push(newItem);
+    res.send("Angelegt");
+  }else{
+    //Overwrite Array index
+    todolist[index] = newItem;
+    res.send("Updated");
+  }
+
+
+  } else {
+    res.send('Error!');
+  }
+
+})
+
+app.listen(3000, function () {
+  console.log('CheckServer listening on port 3000!');
+});
