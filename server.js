302299d974e36a568483b8e69b42bc9f50daee7c
leomaron <maronileo@gmail.com>
2016-03-04 20:50:16 +0100

    Test

--- server.js
+++ server.js
@@ -47,9 +47,5 @@ app.get('/update', function(req, res) {
   }
 
 })
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World from Cloudnode\n\n');
-    res.end();
-}).listen(app_port);
+
 console.log('Web server running at http://' + app_host + ':' + app_port);
