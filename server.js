9e770cbf20166ee54d753099266d330e5cbea27d
leomaron <maronileo@gmail.com>
2016-03-05 12:19:00 +0100

    Test server.js

--- server.js
+++ server.js
@@ -6,52 +6,50 @@ var app_host = process.env.app_host || '127.0.0.1';
 var express = require('express');
 var app = express();
 
-var todolist = [
-  {
-    name:"Bügeln", complete: false,
-    name:"Waschen", complete: false
-  }
-]
-
-app.get('/', function (req, res) {
-    res.send(todolist);
-  });
-
-app.get('/update', function(req, res) {
-  if(req.query.name && req.query.complete) {
-    var index = todolist.findIndex(function(item) {
-      if (item.name == req.query.name) {
-        return true;
-      }else{
-        return false;
-      }
-    })
-
-    var newItem = {
-    name: req.query.name,
-    complete: req.query.complete
-    }
-
-    if (index == -1) {
-    todolist.push(newItem);
-    res.send("Angelegt");
-  }else{
-    //Overwrite Array index
-    todolist[index] = newItem;
-    res.send("Updated");
-  }
-
-
-  } else {
-    res.send('Error!');
-  }
-
-})
 
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World from Cloudnode\n\n');
-    res.end();
+  var todolist = [
+    {
+      name:"Bügeln", complete: false,
+      name:"Waschen", complete: false
+    }
+  ]
+
+  app.get('/', function (req, res) {
+      res.send(todolist);
+    });
+
+  app.get('/update', function(req, res) {
+    if(req.query.name && req.query.complete) {
+      var index = todolist.findIndex(function(item) {
+        if (item.name == req.query.name) {
+          return true;
+        }else{
+          return false;
+        }
+      })
+
+      var newItem = {
+      name: req.query.name,
+      complete: req.query.complete
+      }
+
+      if (index == -1) {
+      todolist.push(newItem);
+      res.send("Angelegt");
+    }else{
+      //Overwrite Array index
+      todolist[index] = newItem;
+      res.send("Updated");
+    }
+
+
+    } else {
+      res.send('Error!');
+    }
+
+  })
 }).listen(app_port);
+
 
 console.log('Web server running at http://' + app_host + ':' + app_port);
