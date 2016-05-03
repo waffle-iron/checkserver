c93c6445a72aa3fb00f63193ce180b71bcf3a0bb
Leo Maroni <maronileo@gmail.com>
2016-05-03 19:54:03 +0200

    Add Firebase Integration

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ var app = express();
 var request = require('request');
 var bodyParser = require('body-parser');
 
+// Firebase
+var Firebase = require("firebase");
+var myFirebaseRef = new Firebase("https://checkserver.firebaseio.com/tiles");
+var myFirebaseRefRoot = new Firebase("https://checkserver.firebaseio.com/");
+
+var FirebaseTokenGenerator = require("firebase-token-generator");
+var tokenGenerator = new FirebaseTokenGenerator(process.env.firebasesecret);
+var token = tokenGenerator.createToken({ uid: "checkserver", some: "arbitrary", data: "here" });
+
+myFirebaseRef.authWithCustomToken(token, function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Login Succeeded!", authData);
+  }
+});
+myFirebaseRefRoot.authWithCustomToken(token, function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Login Succeeded!", authData);
+  }
+});
+
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -24,19 +48,33 @@ var app_host = process.env.app_host || '127.0.0.1';
 app.listen(app_port, app_host, function() {
   console.log("App listen on Port " + app_port)
 })
-
-var todolist = [{
-  name: "Waschen",
-  complete: false,
-  icon: "img/waschen.svg"
-}, {
-  name: "Bügeln",
-  complete: false,
-  icon: "img/buegeln.svg"
-}]
-
 app.get('/', function(req, res) {
-  res.send(todolist);
+var titlelist = '';
+var i = 0;
+  myFirebaseRef.on("value", function(snapshot) {
+    var y = snapshot.val();
+    for(var k in y) {
+    if(y.hasOwnProperty(k)) {
+      i = i + 1;
+      if (i == 1) {
+        titlelist = titlelist + '{'
+        titlelist = titlelist + '"title":"' + k + '"';
+        titlelist = titlelist + ', "status":"' + y[k].status + '"';
+        titlelist = titlelist + ', "icon":"' + y[k].icon + '"';
+        titlelist = titlelist + '}'
+      }
+      else{
+      titlelist = titlelist + ', { "title":"' + k + '"';
+      titlelist = titlelist + ', "status":"' + y[k].status + '"';
+      titlelist = titlelist + ', "icon":"' + y[k].icon + '"';
+      titlelist = titlelist + '}'
+      }
+    }
+}
+})
+var titlelista = JSON.parse('[' + titlelist + ']');
+res.send(titlelista);
+
 });
 
 app.delete('/delete', function(req, res) {
@@ -63,7 +101,7 @@ app.delete('/delete', function(req, res) {
 app.put('/update', function(req, res) {
   var newItem = req.body
 
-  if (newItem.name !== undefined && newItem.complete !== undefined && newItem.icon !== undefined) {
+  if (newItem.name !== undefined && newItem.status !== undefined && newItem.icon !== undefined) {
     var index = todolist.findIndex(function(item) {
       if (item.name == newItem.name) {
         return true;
@@ -74,6 +112,13 @@ app.put('/update', function(req, res) {
 
     if (index == -1) {
       todolist.push(newItem);
+        // Firebase
+       var tilesRef = myFirebaseRef.child(newItem.name);
+       tilesRef.set({
+           title: newItem.name,
+           icon: newItem.icon,
+           status: newItem.status
+       });
       res.send("Angelegt, " + todolist);
     } else {
       //Overwrite Array index
