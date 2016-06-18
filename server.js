ec8c3edb7497ef5c7a672d6ef8b65f7fda9d201e
Leo Maroni <maronileo@gmail.com>
2016-06-19 00:41:29 +0200

    Use New Firebase

--- server.js
+++ server.js
@@ -11,27 +11,23 @@ var BasicStrategy = require('passport-http').BasicStrategy
 var crypto = require('crypto');
 
 // Firebase
-var Firebase = require('firebase')
-var myFirebaseRef = new Firebase('https://checkserver.firebaseio.com/tiles')
-var myFirebaseRefUsers = new Firebase('https://checkserver.firebaseio.com/users')
-
-var FirebaseTokenGenerator = require('firebase-token-generator')
-var tokenGenerator = new FirebaseTokenGenerator(process.env.firebasesecret)
-var token = tokenGenerator.createToken({ uid: 'checkserver', some: 'arbitrary', data: 'here' })
-
-myFirebaseRef.authWithCustomToken(token, function (error, authData) {
-  if (error) {
-    console.log('Login Failed!', error)
-  } else {
-    console.log('Login Succeeded!', authData)
-  }
-})
-myFirebaseRefUsers.authWithCustomToken(token, function (error, authData) {
-  if (error) {
-    console.log('Login Failed!', error)
-  } else {
-    console.log('Login Succeeded!')
-  }
+var firebase = require('firebase')
+var pki = process.env.pki.toString()
+console.log(pki)
+firebase.initializeApp({
+  serviceAccount: {
+    "type": "service_account",
+    "project_id": "project-9135901470923951902",
+    "private_key_id": process.env.pki.toString(),
+    "private_key": process.env.pk.toString(),
+    "client_email": process.env.ce.toString(),
+    "client_id": process.env.ci.toString(),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": process.env.ccu.toString(),
+  },
+  databaseURL: "https://checkserver.firebaseio.com"
 })
 
 // CORS middleware
@@ -54,15 +50,15 @@ var app_host = process.env.app_host || '0.0.0.0'
 app.listen(app_port, app_host, function () {
   console.log('App listen on Port ' + app_port)
 })
-passport.use(new BasicStrategy({
-  },
-  function(username, password, done) {
+passport.use(new BasicStrategy({},
+  function (username, password, done) {
     process.nextTick(function () {
-      myFirebaseRefUsers.on('value', function (snapshot) {
-      var y = snapshot.val()
+      var usersref = firebase.database().ref('users')
+      usersref.once('value', function (snapshot) {
+        var y = snapshot.val()
         if (y.hasOwnProperty(username)) {
           var md5pass = crypto.createHash('md5').update(password).digest('hex');
-          if(md5pass === y[username].pass) {
+          if (md5pass === y[ username ].pass) {
             done(null, true)
           } else {
             done(null, false)
@@ -70,7 +66,7 @@ passport.use(new BasicStrategy({
         } else {
           done(null, false)
         }
-    })
+      })
     })
   }
 ));
@@ -78,37 +74,41 @@ passport.use(new BasicStrategy({
 app.get('/', passport.authenticate('basic', { session: false }), function (req, res) {
   var titlelist = ''
   var i = 0
-  myFirebaseRef.on('value', function (snapshot) {
+  var tilesref = firebase.database().ref('tiles')
+  tilesref.once('value', function (snapshot) {
     var y = snapshot.val()
     for (var k in y) {
       if (y.hasOwnProperty(k)) {
         i = i + 1
         if (i === 1) {
-          titlelist = titlelist + '{'
+          titlelist = '{'
           titlelist = titlelist + '"title":"' + k + '"'
-          titlelist = titlelist + ', "status":"' + y[k].status + '"'
-          titlelist = titlelist + ', "icon":"' + y[k].icon + '"'
+          titlelist = titlelist + ', "status":"' + y[ k ].status + '"'
+          titlelist = titlelist + ', "icon":"' + y[ k ].icon + '"'
           titlelist = titlelist + '}'
         } else {
           titlelist = titlelist + ', { "title":"' + k + '"'
-          titlelist = titlelist + ', "status":"' + y[k].status + '"'
-          titlelist = titlelist + ', "icon":"' + y[k].icon + '"'
+          titlelist = titlelist + ', "status":"' + y[ k ].status + '"'
+          titlelist = titlelist + ', "icon":"' + y[ k ].icon + '"'
           titlelist = titlelist + '}'
         }
       }
     }
+    var titlelista = JSON.parse('[' + titlelist + ']')
+    res.send(titlelista)
   })
-  var titlelista = JSON.parse('[' + titlelist + ']')
-  res.send(titlelista)
 })
 
 app.delete('/delete', passport.authenticate('basic', { session: false }), function (req, res) {
   var delItem = req.body
   // Prüfung ob alle Daten da sind
   if (delItem.name !== undefined) {
-    var tilesRef = myFirebaseRef.child(delItem.name)
-    tilesRef.remove()
-    res.send("Entfernt")
+    var tilesRef = firebase.database().ref('tiles')
+    var tilesChild = tilesRef.child(delItem.name)
+    tilesChild.remove()
+      .then(function () {
+        res.send("Entfernt")
+      })
   } else {
     res.send('Error of Data!')
   }
@@ -118,8 +118,9 @@ app.put('/update', passport.authenticate('basic', { session: false }), function 
   var newItem = req.body
   if (newItem.name !== undefined && newItem.status !== undefined && newItem.icon !== undefined) {
     // Firebase
-    var tilesRef = myFirebaseRef.child(newItem.name)
-    tilesRef.set({
+    var tilesRef = firebase.database().ref('tiles')
+    var tilesChild = tilesRef.child(newItem.name)
+    tilesChild.set({
       title: newItem.name,
       icon: newItem.icon,
       status: newItem.status
@@ -150,52 +151,56 @@ app.put('/register', passport.authenticate('basic', { session: false }), functio
   var data = req.body
   if (data.pass !== undefined && data.name !== undefined && data.email !== undefined) {
     var regun = 1
-    myFirebaseRefUsers.on('value', function (snapshotu) {
-      var v = snapshotu.val()
+    var usersRef = firebase.database().ref('users')
+    usersRef.once('value', function (snapshot) {
+      var v = snapshot.val()
       var regi = 1
       while (v.hasOwnProperty(regi)) {
         regun++
         regi++
       }
-    })
-    res.send('' + regun)
-    var usersRef = myFirebaseRefUsers.child(regun)
-    usersRef.set({
-      pass: data.pass,
-      name: data.name,
-      email: data.email
+      res.send('' + regun)
+      var usersChild = usersRef.child(regun)
+      usersChild.set({
+        pass: data.pass,
+        name: data.name,
+        email: data.email
+      })
     })
   } else {
     res.send('Data Error')
   }
 })
-app.put('/userdata', passport.authenticate('basic', { session: false}), function (req, res) {
+app.put('/userdata', passport.authenticate('basic', { session: false }), function (req, res) {
   var data = req.body
   if (data.email !== undefined && data.pass !== undefined) {
     var userdata = ''
     var passr = false
-    myFirebaseRefUsers.on('value', function (snapshot) {
+    var usersRef = firebase.database().ref('users')
+    usersRef.once('value', function (snapshot) {
       var y = snapshot.val()
       for (var k in y) {
         if (y.hasOwnProperty(k)) {
-          if(y[k].email === data.email) {
-            if(y[k].pass === data.pass) {
+          if (y[ k ].email === data.email) {
+            if (y[ k ].pass === data.pass) {
               passr = true
-              userdata = userdata + '{'
+              userdata = '{'
               userdata = userdata + '"username":"' + k + '"'
-              userdata = userdata + ', "email":"' + y[k].email + '"'
-              userdata = userdata + ', "name":"' + y[k].name + '"'
+              userdata = userdata + ', "email":"' + y[ k ].email + '"'
+              userdata = userdata + ', "name":"' + y[ k ].name + '"'
               userdata = userdata + '}'
-           }
+            }
           }
         }
       }
+      if (passr) {
+        var userdataa = JSON.parse('[' + userdata + ']')
+        res.send(userdataa)
+      } else {
+        res.sendStatus(400)
+      }
     })
-    if(passr) {
-    var userdataa = JSON.parse('[' + userdata + ']')
-    res.send(userdataa)
   } else {
-    res.sendStatus(400)
-  }
+    res.send('Data Error')
   }
 })
