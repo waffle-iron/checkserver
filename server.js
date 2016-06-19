bb99fe71e05e40e6734a375f8960d3e79ac6f8bd
Leo Maroni <maronileo@gmail.com>
2016-06-19 17:13:54 +0200

    mend

--- server.js
+++ server.js
@@ -15,18 +15,7 @@ var firebase = require('firebase')
 var pki = process.env.pki.toString()
 console.log(pki)
 firebase.initializeApp({
-  serviceAccount: {
-    "type": "service_account",
-    "project_id": "project-9135901470923951902",
-    "private_key_id": process.env.pki.toString(),
-    "private_key": process.env.pk.toString(),
-    "client_email": process.env.ce.toString(),
-    "client_id": process.env.ci.toString(),
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://accounts.google.com/o/oauth2/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": process.env.ccu.toString(),
-  },
+  serviceAccount: "firebase.json",
   databaseURL: "https://checkserver.firebaseio.com"
 })
 
